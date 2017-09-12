875f3496033deeca4c0563bd216551a8b4f3ed07
Mantas Paulauskas <mantopastas@gmail.com>
2017-09-12 12:02:32 +0300

    removed order payment  status translations from order states translations

--- src/resources/lang/en/e_commerce_orders_states.php
+++ src/resources/lang/en/e_commerce_orders_states.php
@@ -4,9 +4,6 @@ return [
     'page_title' => 'Order states',
     'title'      => 'Title',
 
-    'awaiting-bank-wire-payment' => 'Awaiting bank wire payment',
-    'payment-accepted'           => 'Payment accepted',
-
     'ready-for-processing'   => 'Ready for processing',
     'processing-in-progress' => 'Processing in progress',
     'shipped'                => 'Shipped',
--- src/resources/lang/lt/e_commerce_orders_states.php
+++ src/resources/lang/lt/e_commerce_orders_states.php
@@ -4,9 +4,6 @@ return [
     'page_title' => 'Užsakymo būsenos',
     'title'      => 'Pavadinimas',
 
-    'awaiting-bank-wire-payment' => 'Laukiama apmokėjimo pavedimu',
-    'payment-accepted'           => 'Mokėjimas gautas',
-
     'ready-for-processing'   => 'Užsakymas paruoštas vykdymui',
     'processing-in-progress' => 'Užsakymas vykdomas',
     'shipped'                => 'Užsakymas išsiųstas',
