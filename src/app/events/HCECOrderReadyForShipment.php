b9393083798069341c98a6aa27172ba7480fdabd
Mantas Paulauskas <mantopastas@gmail.com>
2017-09-13 15:27:30 +0300

    added events for order states and payment status

---
+++ src/app/events/HCECOrderReadyForShipment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace interactivesolutions\honeycombecommerceorders\app\events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use interactivesolutions\honeycombecommerceorders\app\models\ecommerce\HCECOrders;
+
+class HCECOrderReadyForShipment
+{
+    use SerializesModels;
+
+    /**
+     * @var HCECOrders
+     */
+    public $order;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param HCECOrders $order
+     */
+    public function __construct(HCECOrders $order)
+    {
+        $this->order = $order;
+    }
+}
