e705354859ecfc4cf4250ddf60e8c73517e17e5c
Mantas Paulauskas <mantopastas@gmail.com>
2017-09-21 13:07:36 +0300

    added translations

---
+++ src/resources/lang/lt/e_commerce_orders_discount_codes.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'page_title'        => 'Užsakymo nulaidos kodai',
+    'order_id'          => 'Užsakymas',
+    'title'             => 'Pavadinimas',
+    'code'              => 'Kodas',
+    'type'              => 'Tipas',
+    'amount'            => 'Kiekis',
+    'shipping_included' => 'Įskaitant pristatymą',
+    'free_shipping'     => 'Nemokamas pristatymas',
+];
