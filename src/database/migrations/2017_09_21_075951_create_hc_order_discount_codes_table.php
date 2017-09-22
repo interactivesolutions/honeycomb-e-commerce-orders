151e5c4cd7f42569a5ee26f4bf0918de86f6f078
Mantas Paulauskas <mantopastas@gmail.com>
2017-09-22 16:26:25 +0300

    unique fix

--- src/database/migrations/2017_09_21_075951_create_hc_order_discount_codes_table.php
+++ src/database/migrations/2017_09_21_075951_create_hc_order_discount_codes_table.php
@@ -21,7 +21,7 @@ class CreateHcOrderDiscountCodesTable extends Migration
 
             $table->string('order_id', 36);
             $table->string('title')->nullable();
-            $table->string('code', 45)->unique('unique_code');
+            $table->string('code', 45);
             $table->enum('type', ['percentage', 'fixed', 'none'])->default('none');
             $table->float('amount', 20, 8)->default('0.000000')->nullable();
             $table->enum('shipping_included', ['1', '0'])->default('0');
