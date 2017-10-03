a106878d9de33d8b3aa0d7959af4c4d1f66c8633
Mantas Paulauskas <mantopastas@gmail.com>
2017-10-03 09:55:39 +0300

    updated error text

--- src/resources/lang/lt/e_commerce_carts.php
+++ src/resources/lang/lt/e_commerce_carts.php
@@ -8,7 +8,7 @@ return [
     'errors' => [
         'not_found'               => 'Kažkas negerai su krepšeliu!',
         'item_not_found'          => 'Kažkas negerai su krepšelio preke!',
-        'not_enough'              => 'Nepakankamas prekės likutis. Galima pirkti :available prekes(-e,ių)',
-        'not_enough_to_pre_order' => 'Nepakankamas prekės likutis. Galite nusipirkti daugiausiai :on_sale prekes(-e,ių) arba rezervuoti daugiausiai :pre_order prekes(-e,ių)',
+        'not_enough'              => 'Nepakankamas prekės likutis. Galima pirkti :available vnt.',
+        'not_enough_to_pre_order' => 'Nepakankamas prekės likutis. Šiuo metu iš karto galite įsigyti :on_sale vnt. arba rezervuoti :pre_order vnt.',
     ],
 ];
